67ccbc14aaf88eb9aecf10f7b6027f806e381ff0
Tumanov-Nikita <33156396+Tumanov-Nikita@users.noreply.github.com>
2017-12-23 10:08:53 +0400

    Update Stove.java

--- src/Stove.java
+++ src/Stove.java
@@ -2,14 +2,14 @@
 public class Stove {
 	private Pan pan;
     public boolean State;
-    public Pan Pan;// { set { pan = value; } get { return pan; } }
+    public Pan Pan;
     
     public void setPan (Pan value){
-    	pan=value;                     //{ set { pan = value; }
+    	pan=value;                 
     }
     
     public Pan getPan (){
-    	return pan;             // get { return pan; }
+    	return pan;    
     }
         
     public void Cook()
