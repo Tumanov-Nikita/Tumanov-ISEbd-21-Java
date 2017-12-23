a66af3cdd19833571296c505b2b255d61bc1bb94
Tumanov-Nikita <33156396+Tumanov-Nikita@users.noreply.github.com>
2017-12-23 10:07:51 +0400

    Update Egg.java

--- src/Egg.java
+++ src/Egg.java
@@ -1,7 +1,6 @@
 
 public class Egg {
 	 public int countEggs;
-   //  public int has_ready = 0;
      private int dirty = 10;
      public boolean Have_salt;
 
@@ -16,7 +15,7 @@ public class Egg {
          return this.Dirty;
       }
       public void setDirty(int dirty) { 
-    	  if (dirty > -1 && dirty < 11) //{ set { if (value > -1 && value < 11) dirty = value; } get { return dirty; } }
+    	  if (dirty > -1 && dirty < 11)
          this.Dirty=dirty;
       }
       
